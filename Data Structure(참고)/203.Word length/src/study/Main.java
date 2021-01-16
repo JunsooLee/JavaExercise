08cd880ff9616530bab0ed4b3812cd4f437e7946
JunsooLee <junsoolee65@naver.com>
2021-01-16 23:09:51 +0900

    2021.01.16 Algorithms
    Data Structure Algorithms

---
+++ Data Structure(참고)/203.Word length/src/study/Main.java
@@ -0,0 +1,21 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		
+		bw.append(Integer.toString(str.length()));
+		bw.flush();
+	}
+
+}
